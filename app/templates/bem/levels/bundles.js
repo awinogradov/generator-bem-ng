183825c0d76efb16a55c22a59bb04a9abb130ad4
Anton Winogradov <winogradovaa@gmail.com>
2014-02-18 22:51:36 +0400

    Move blocks inheritance for bundle from level.js in *.bundles/.bem

--- app/templates/bem/levels/bundles.js
+++ app/templates/bem/levels/bundles.js
@@ -1,4 +1,5 @@
 var PATH = require('path'),
+    BEM = require('bem'),
     environ = require('bem-environ')({ libDir: 'bower_components' }),
     join = PATH.join,
 
@@ -32,3 +33,26 @@ exports.getTechs = function() {
 
 // Create bundles in bemjson.js tech
 exports.defaultTechs = ['bemjson.js'];
+
+exports.getConfig = function() {
+
+    return BEM.util.extend(this.__base() || {}, {
+        bundleBuildLevels: this.resolvePaths(
+            [
+                // Include Yandex maintainable blocks
+                'bem-core/common.blocks',
+                'bem-core/desktop.blocks',
+                'bem-components/common.blocks',
+                'bem-components/desktop.blocks',
+                // Include examples
+                'webapp-bem-example/example.blocks'
+            ]
+            .map(function(path) { return PATH.resolve(environ.LIB_ROOT, path); })
+            // Include custom blocks
+            .concat([
+                'app.blocks'
+            ]
+            .map(function(path) { return PATH.resolve(environ.PRJ_ROOT, path); })))
+    });
+
+};
