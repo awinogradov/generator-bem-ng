5cf16c2211d0438653edc9e8b51c22ea3f6f09d0
Anton Winogradov <winogradovaa@gmail.com>
2014-02-19 00:58:35 +0400

    More pretty

---
+++ app/templates/_make.js
@@ -0,0 +1,48 @@
+/* jshint node:true */
+/* global MAKE */
+
+// override default 'libs' directory for compatibility with Bower
+var environ = require('bem-environ')({ libDir: '<%= _.slugify(project.libs) %>' });
+
+environ.extendMake(MAKE);
+
+//process.env.YENV = 'production';
+//process.env.XJST_ASYNCIFY = 'yes';
+
+MAKE.decl('Arch', {
+
+    blocksLevelsRegexp: /^.+?\.blocks/,
+    bundlesLevelsRegexp: /^.+?\.bundles$/,
+
+    libraries: [
+        'bem-core @ f4b46ef0590549042d938f7e981df4d14eb4caef',
+        'bem-components @ 82301a8af6c15c2849d1f755a24f594de6522251'
+    ]
+
+});
+
+
+MAKE.decl('BundleNode', {
+
+    getTechs: function() {
+
+        return [
+            'bemjson.js',
+            'bemdecl.js',
+            'deps.js',
+            'bemhtml',
+            'browser.js+bemhtml',
+            'css',
+            'ie.css',
+            'html'
+        ];
+
+    },
+
+    'create-browser.js+bemhtml-optimizer-node': function(tech, sourceNode, bundleNode) {
+        sourceNode.getFiles().forEach(function(f) {
+            this['create-js-optimizer-node'](tech, this.ctx.arch.getNode(f), bundleNode);
+        }, this);
+    }
+
+});
