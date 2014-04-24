9b5b1671a822bfbcf466be25d4e9ad97380a9ccb
Anton Winogradov <winogradovaa@gmail.com>
2014-04-24 10:59:57 +0400

    awesome stub with separated disign and platforms

--- app/index.js
+++ app/index.js
@@ -7,48 +7,41 @@ var cwd    = process.cwd(),
     yeoman = require('yeoman-generator');
 
 var BemGenerator = module.exports = function BemGenerator(args, options, config) {
-
     yeoman.generators.Base.apply(this, arguments);
 
     this.pkg      = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
     this.email    = this.user.git.email;
     this.username = this.user.git.username || this.shell.exec('whoami').output.trim();
 
+    var _this = this;
+
+    this.on('end', function () {
+
+        if (_this.options['skip-install']) {
+            return;
+        }
+
+        _this.installDependencies({
+            skipMessage: _this.options['skip-install-message'],
+            skipInstall: _this.options['skip-install'],
+            bower: false,
+            callback: function() {
+                _this.log.write('').ok('Done!');
+                setTimeout(process.exit, 0, 0);
+            }
+        });
+
+    });
 };
 
 util.inherits(BemGenerator, yeoman.generators.Base);
-
-BemGenerator.prototype.dots = function git() {
-    this.directory('dots', cwd);
-};
 
 BemGenerator.prototype.packages = function packageJSON() {
     this.template('_package.json', 'package.json');
     this.template('_bower.json', 'bower.json');
 };
 
-BemGenerator.prototype.install = function () {
-    if (this.options['skip-install']) {
-        return;
-    }
-
-    this.on('end', function () {
-        this.installDependencies({
-            skipMessage: this.options['skip-install-message'],
-            skipInstall: this.options['skip-install'],
-            bower: false,
-            callback: function() {
-                var bowerNpmInstall = require(join(cwd, 'node_modules', 'bower-npm-install'));
-                // call npm install for every bower component
-                console.log("\nInstall npm dependencies for bower_components...\n");
-                bowerNpmInstall()
-                    .on('error', function(err) { console.error(err); })
-                    .on('end', function(installed) {
-                        console.log('Installed packages: ' + JSON.stringify(installed, null, 2));
-                        // Go to make structure
-                        this.invoke('bem:init', {options: {nested: true, appName: this.appName}});
-                    }.bind(this));
-            }.bind(this)
-        });
-    });
+BemGenerator.prototype.app = function app() {
+    this.directory('tree', cwd);
+    this.template('_README.md', 'README.md');
 };
